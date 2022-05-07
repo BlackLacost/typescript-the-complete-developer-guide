792b4c224d023b0516f48acfdb2b25282ac34393
Ilya Lisin <blacklacost@gmail.com>
2022-05-07 21:36:05 +0300

    13 наследование User из Model

--- 13 web/src/index.ts
+++ 13 web/src/index.ts
@@ -1,11 +1,7 @@
 import { User } from './models/User'
 
-const user = new User({ id: 1, name: 'newer name', age: 0 })
+const user = User.buildUser({ id: 1 })
 
-user.on('save', () => console.log(user))
-// user.on('change', () => console.log('change1'))
-// user.trigger('change')
+user.on('change', () => console.log(user))
 
-user.save()
-
-// Reminer on how 'this' works in javascript
+user.fetch()
