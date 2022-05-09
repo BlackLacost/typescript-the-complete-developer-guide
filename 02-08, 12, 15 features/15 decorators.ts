2897b59cbf0622f262b23ede89c01860fbdbf264
Ilya Lisin <blacklacost@gmail.com>
2022-05-09 14:10:02 +0300

    15 Decorators

---
+++ 02-08, 12, 15 features/15 decorators.ts
@@ -0,0 +1,47 @@
+@classDecorator
+class Boat {
+  color: string = 'red'
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`
+  }
+
+  @logError('Oops, boat was sunk')
+  pilot(@parameterDecorator speed: string): void {
+    if (speed === 'fast') {
+      console.log('swish')
+    } else {
+      console.log('nothing')
+    }
+  }
+}
+
+// logError(Boat.prototype, 'pilot')
+
+// function testDeacorator(target: any, key: string, desc: PropertyDescriptor) {
+//   console.log(key)
+// }
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor)
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index)
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value
+
+    desc.value = function () {
+      try {
+        method()
+      } catch (e) {
+        console.log(errorMessage)
+      }
+    }
+  }
+}
+
+// new Boat().pilot()
